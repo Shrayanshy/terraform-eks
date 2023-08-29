9ca3242ccc4511e97efd35ccccae7d0e39dd2ddf
Shrayansh Yadav <shrayanshy@gmail.com>
2023-08-29 16:03:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-xxxxxxxxxxxxxxxxx", "subnet-yyyyyyyyyyyyyyyyy"]  
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_eks_node_group" "my_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  launch_template {
+    id      = aws_launch_template.eks_template.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_iam_role" "eks_nodes" {
+  name = "eks-nodes-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_launch_template" "eks_template" {
+  name_prefix   = "eks-template-"
+  image_id      = "ami-xxxxxxxxxxxxxxxxx"  
+  instance_type = "t2.micro"               
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 20
+    }
+  }
+}
